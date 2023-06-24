e93bf17d6c2f4185e7a0d4c841e4ec341567501f
DarkStar☀ <61005029+Twin-Flame@users.noreply.github.com>
2023-06-24 03:33:08 -0500

    Update set.sh

--- Focus follows Mouse/set.sh
+++ Focus follows Mouse/set.sh
@@ -1,3 +1,5 @@
 gsettings set org.gnome.desktop.wm.preferences focus-mode 'sloppy'
 
 gsettings set org.gnome.desktop.wm.preferences auto-raise-delay 0
+
+gsettings set org.gnome.desktop.wm.preferences auto-raise true
